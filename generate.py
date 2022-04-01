3eb1a854250ad0fe96b6b348a97064caee48d9d7
Laddge <dev.laddge@gmail.com>
2022-04-01 23:48:23 +0000

    [feat] add custom font

--- generate.py
+++ generate.py
@@ -3,8 +3,10 @@ import os
 import re
 import shutil
 
+import fontTools.subset
 import reducss
 import sass
+from bs4 import BeautifulSoup
 
 
 def generate():
@@ -34,6 +36,26 @@ def generate():
 
     # reduce css
     reducss.auto(distdir)
+
+    # subset fonts
+    htmls = glob.glob(os.path.join(distdir, "**/*.html"), recursive=True)
+    subset_chars = ""
+    for html in htmls:
+        with open(html) as f:
+            htmlstr = f.read()
+        soup = BeautifulSoup(htmlstr, "html.parser")
+        text = "".join([el.get_text(strip=True) for el in soup.select(".mplus2")])
+        subset_chars += text
+    subset_chars = "".join(set(subset_chars))
+    font = "MPLUS2-Bold.ttf"
+    output = font.replace(".ttf", ".woff2").lower()
+    args = []
+    args.append(os.path.join(srcdir, f"fonts/.ignore/{font}"))
+    args.append(f"--text={subset_chars}")
+    args.append("--flavor=woff2")
+    args.append("--output-file=" + os.path.join(distdir, f"fonts/{output}"))
+    os.makedirs(os.path.join(distdir, "fonts"), exist_ok=True)
+    fontTools.subset.main(args)
 
 
 if __name__ == "__main__":
