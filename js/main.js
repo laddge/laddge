ae18b2c097ff21c31fd791fcd8492b697f4f2407
laddge <laddge@users.noreply.github.com>
2022-05-14 10:35:17 +0000

    deploy: d93a02b28dc2c691b441239fc0253698c5b59f8a

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-const cardAPIInstance = new CardAPI('cardapi', '\
-    <div class="card mt-3 me-3 d-inline-block" style="width: 18rem;">\
-    <img class="bd-placeholder-img card-img-top w-100" src="{{ image }}" style="width: 18rem; height: 9rem;" alt="">\
-    <div class="card-body">\
-    <div class="card-title w-100 text-truncate my-0 h3">{{ title }}</div>\
-    <div class="card-text w-100 text-truncate-2 text-secondary small mt-1">{{ description }}</div>\
-    <div class="text-end">\
-    <a href="{{ href }}" class="btn btn-outline-primary btn-sm mt-3">Open</a>\
-    </div>\
-    </div>\
-    </div>\
-    ');
-cardAPIInstance.load();
 if (document.defaultView.getComputedStyle(document.getElementById('sidebar'), null).display != 'none') {
     fetch('https://api.laddge.net/profile')
         .then(res => {
@@ -31,3 +18,31 @@ if (document.defaultView.getComputedStyle(document.getElementById('sidebar'), nu
             console.log(err);
         });
 }
+
+let cardLoaded = false;
+
+function loadCards() {
+    if (cardLoaded) {
+        return;
+    }
+    const cardAPIInstance = new CardAPI('cardapi', '\
+        <div class="card mt-3 me-3 d-inline-block" style="width: 18rem;">\
+        <img class="bd-placeholder-img card-img-top w-100" src="{{ image }}" style="width: 18rem; height: 9rem;" alt="">\
+        <div class="card-body">\
+        <div class="card-title w-100 text-truncate my-0 h3">{{ title }}</div>\
+        <div class="card-text w-100 text-truncate-2 text-secondary small mt-1">{{ description }}</div>\
+        <div class="text-end">\
+        <a href="{{ href }}" class="btn btn-outline-primary btn-sm mt-3">Open</a>\
+        </div>\
+        </div>\
+        </div>\
+        ');
+    cardAPIInstance.load();
+    cardLoaded = true;
+}
+
+if (document.getElementById('products').getBoundingClientRect().top + window.pageYOffset < document.documentElement.scrollTop) {
+    loadCards();
+}
+
+window.addEventListener('scroll', loadCards);
