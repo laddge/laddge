5b0ac55568d0d896249b2503e53d8b3444dea47e
laddge <laddge@users.noreply.github.com>
2022-05-14 21:56:52 +0000

    deploy: 01651d708f768634e1eff745526ec5485baa0b94

--- js/main.js
+++ js/main.js
@@ -1,36 +1,7 @@
-if (document.defaultView.getComputedStyle(document.getElementById('sidebar'), null).display != 'none') {
-    fetch('https://api.laddge.net/profile')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            document.getElementById('twitterBanner').src = data.profile_banner;
-            document.getElementById('twitterImage').src = data.profile_image;
-            document.getElementById('twitterName').innerText = data.name;
-            document.getElementById('twitterScreenName').innerText = '@' + data.screen_name;
-            document.getElementById('twitterDesc').innerText = data.desc;
-            document.getElementById('twitterFollow').innerText += data.follow;
-            document.getElementById('twitterFollower').innerText += data.follower;
-            const el = document.createElement('script');
-            el.onload = function () {
-                twemoji.parse(document.getElementById('twitterCardBody'));
-            }
-            el.src= 'https://twemoji.maxcdn.com/v/latest/twemoji.min.js';
-            document.body.appendChild(el);
-            document.getElementById('twitterCard').classList.remove('d-none');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-}
-
-let cardLoaded = false;
-
-function loadCards() {
-    if (cardLoaded) {
-        return;
-    }
-    const cardAPIInstance = new CardAPI('cardapi', '\
+if(document.defaultView.getComputedStyle(document.getElementById('sidebar'),null).display!='none'){fetch('https://api.laddge.net/profile').then(res=>{return res.json();}).then(data=>{document.getElementById('twitterBanner').src=data.profile_banner;document.getElementById('twitterImage').src=data.profile_image;document.getElementById('twitterName').innerText=data.name;document.getElementById('twitterScreenName').innerText='@'+data.screen_name;document.getElementById('twitterDesc').innerText=data.desc;document.getElementById('twitterFollow').innerText+=data.follow;document.getElementById('twitterFollower').innerText+=data.follower;const el=document.createElement('script');el.onload=function(){twemoji.parse(document.getElementById('twitterCardBody'));}
+el.src='https://twemoji.maxcdn.com/v/latest/twemoji.min.js';document.body.appendChild(el);document.getElementById('twitterCard').classList.remove('d-none');}).catch(err=>{console.log(err);});}
+let cardLoaded=false;function loadCards(){if(cardLoaded){return;}
+const cardAPIInstance=new CardAPI('cardapi','\
         <div class="card mt-3 me-3 d-inline-block" style="width: 18rem;">\
         <img class="bd-placeholder-img card-img-top w-100" src="{{ image }}" style="width: 18rem; height: 9rem;" alt="">\
         <div class="card-body">\
@@ -41,13 +12,6 @@ function loadCards() {
         </div>\
         </div>\
         </div>\
-        ');
-    cardAPIInstance.load();
-    cardLoaded = true;
-}
-
-if (document.getElementById('products').getBoundingClientRect().top + window.pageYOffset < window.innerHeight) {
-    loadCards();
-}
-
-window.addEventListener('scroll', loadCards);
+        ');cardAPIInstance.load();cardLoaded=true;}
+if(document.getElementById('products').getBoundingClientRect().top+window.pageYOffset<window.innerHeight){loadCards();}
+window.addEventListener('scroll',loadCards);
